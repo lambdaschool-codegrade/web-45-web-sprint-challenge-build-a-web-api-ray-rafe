8f26ab965afedca639e63e90b2b586833b5b3ac6
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-09-03 15:42:18 -0600

    mvp

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,16 @@
-// add middlewares here related to actions
+const _ = require("lodash");
+
+const validateBody = function (req, res, next) {
+  if (
+    _.isNil(req.body.description) ||
+    _.isNil(req.body.notes) ||
+    _.isNil(req.body.project_id) ||
+    _.isNil(req.body.completed)
+  ) {
+    return res.sendStatus(400);
+  }
+
+  next();
+};
+
+module.exports.validateBody = validateBody;
--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,15 @@
-// add middlewares here related to projects
+const _ = require("lodash");
+
+const validateBody = function (req, res, next) {
+  if (
+    _.isNil(req.body.name) ||
+    _.isNil(req.body.description) ||
+    _.isNil(req.body.completed)
+  ) {
+    return res.sendStatus(400);
+  }
+
+  next();
+};
+
+module.exports.validateBody = validateBody;
